cbcf16ac0a701b6d141ef297f97a43acfc77d331
Abbasmanthiri <abbasrahmanic1997@gmail.com>
2022-09-23 12:12:40 +0530

    new code

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -3,7 +3,9 @@ package org.test;
 public class A {
 	  static int id=100;
 	public static void m1() {
-	System.out.println(id);	 
+	System.out.println(id);	
+	System.out.println(id);	
+	
 	}
 	private static void m2() {
 		m1();
